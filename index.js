ba2a9298c4634e33fc918385648f888b397c45d6
Megan McLean <425megs@gmail.com>
2022-01-20 17:53:49 -0800

    inital, skeletons set up

---
+++ index.js
@@ -0,0 +1,35 @@
+// TODO: Include packages needed for this application
+    //Inquirer package
+const inquirer = require('inquirer');
+const generater = require('./utils/generateMarkdown.js');
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+// title of project
+    // type: 
+    // message: 
+    // name:
+// what the app is for
+// how to use the app
+// motivation for creating
+// how to install it
+// how to report issues
+// how to make contributions
+// technologies used
+// what I learned
+// future plans
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+
+//title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,36 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) 
+// {
+  // if () {
+//     return license link
+//   } else {
+//     return ("")
+//   }
+// }
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) 
+  // {
+      // if () {
+//     return license section
+//   } else {
+//     return ("")
+//   }
+// }
+
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
